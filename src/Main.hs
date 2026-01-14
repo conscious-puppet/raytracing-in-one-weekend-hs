d3afabb8f519297b2e542aba71a8871972285f67
Abhishek Singh <abhishek17513@gmail.com>
2026-01-14 17:46:42 +0530

    4. Rays, a Simple Camera, and Background

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
 module Main (main) where
 
 import Data.Text.IO qualified as T
@@ -14,6 +16,8 @@ import ListT qualified
 
 newtype ImageWidth = ImageWidth Int
 newtype ImageHeight = ImageHeight Int
+newtype ViewportWidth = ViewportWidth Double
+newtype ViewportHeight = ViewportHeight Double
 newtype ImagePixel = ImagePixel (Int, Int, Int)
 newtype ImageHandleErr = ImageHandleErr Text deriving stock (Show)
 
@@ -25,24 +29,27 @@ data Image :: E.Effect where
 
 E.makeEffect ''Image
 
-runImage ::
-  (IOE :> es, E.Error ImageHandleErr :> es, E.FileSystem :> es) =>
-  Eff (Image : es) a ->
-  Eff es a
+runImage
+  :: (E.Error ImageHandleErr :> es, E.FileSystem :> es, IOE :> es)
+  => Eff (Image : es) a
+  -> Eff es a
 runImage = E.reinterpret (E.evalState (Nothing :: Maybe Handle)) $ const $ \case
   OpenImage filePath -> do
     imageHandle <- E.openFile filePath WriteMode
     E.put (Just imageHandle)
   CloseImage -> do
-    imageHandle <- E.get `whenNothingM` (E.throwError . ImageHandleErr $ "No image opened. Use `openImage` first.")
+    imageHandle <-
+      E.get `whenNothingM` (E.throwError . ImageHandleErr $ "No image opened. Use `openImage` first.")
     E.hFlush imageHandle >> E.hClose imageHandle
   InitImage (ImageWidth imageWidth') (ImageHeight imageHeight') -> do
-    imageHandle <- E.get `whenNothingM` (E.throwError . ImageHandleErr $ "No image opened. Use `openImage` first.")
+    imageHandle <-
+      E.get `whenNothingM` (E.throwError . ImageHandleErr $ "No image opened. Use `openImage` first.")
     liftIO $ T.hPutStrLn imageHandle "P3"
     liftIO . T.hPutStrLn imageHandle $ show imageWidth' <> " " <> show imageHeight'
     liftIO $ T.hPutStrLn imageHandle "255"
   PutPixelLn (ImagePixel (r, g, b)) -> do
-    imageHandle <- E.get `whenNothingM` (E.throwError . ImageHandleErr $ "No image opened. Use `openImage` first.")
+    imageHandle <-
+      E.get `whenNothingM` (E.throwError . ImageHandleErr $ "No image opened. Use `openImage` first.")
     liftIO $ T.hPutStrLn imageHandle $ show r <> " " <> show g <> " " <> show b
 
 data Logger :: E.Effect where
@@ -51,10 +58,7 @@ data Logger :: E.Effect where
 
 E.makeEffect ''Logger
 
-runLogger ::
-  (IOE :> es) =>
-  Eff (Logger : es) a ->
-  Eff es a
+runLogger :: IOE :> es => Eff (Logger : es) a -> Eff es a
 runLogger = E.interpret $ const $ \case
   Log content -> liftIO $ putText content
   LogLn content -> liftIO $ putTextLn content
@@ -68,13 +72,13 @@ instance Num Vec3 where
   negate (Vec3 (x, y, z)) = Vec3 (negate x, negate y, negate z)
   abs (Vec3 (x, y, z)) = Vec3 (abs x, abs y, abs z)
   signum (Vec3 (x, y, z)) = Vec3 (signum x, signum y, signum z)
-  fromInteger i = let iD = fromInteger i in Vec3 (iD, iD, iD)
+  fromInteger i = let iV = fromInteger i in Vec3 (iV, iV, iV)
 
 class Vector3 v where
   xVal :: v -> Double
   yVal :: v -> Double
   zVal :: v -> Double
-  scale :: Double -> v -> v
+  scale :: v -> Double -> v
   vLength :: v -> Double
   lengthSquared :: v -> Double
   toText :: v -> Text
@@ -86,31 +90,48 @@ instance Vector3 Vec3 where
   xVal (Vec3 (x, _, _)) = x
   yVal (Vec3 (_, y, _)) = y
   zVal (Vec3 (_, _, z)) = z
-  scale k vec = Vec3 (k, k, k) * vec
+  scale vec k = vec * Vec3 (k, k, k)
   vLength vec = sqrt $ lengthSquared vec
   lengthSquared vec = dot vec vec
   toText (Vec3 (x, y, z)) = show x <> " " <> show y <> " " <> show z
   dot (Vec3 (x1, y1, z1)) (Vec3 (x2, y2, z2)) = x1 * x2 + y1 * y2 + z1 * z2
   cross (Vec3 (x1, y1, z1)) (Vec3 (x2, y2, z2)) = Vec3 (y1 * z2 - y2 * z1, z1 * x2 - z2 * x1, x1 * y2 - y1 * x2)
-  unitVector vec = scale (1 / vLength vec) vec
+  unitVector vec = vec `scale` (1 / vLength vec)
 
-newtype Color = Color Vec3
-  deriving (Num) via Vec3
-  deriving (Vector3) via Vec3
+newtype Color = Color (Double, Double, Double)
+  deriving (Num, Vector3) via Vec3
 
-class ColorRGB v where
-  pixel :: v -> ImagePixel
+class ColorRGB c where
+  rVal :: c -> Double
+  gVal :: c -> Double
+  bVal :: c -> Double
+  pixel :: c -> ImagePixel
 
 instance ColorRGB Color where
-  pixel (Color (Vec3 (r, g, b))) =
+  rVal = xVal
+  gVal = yVal
+  bVal = zVal
+  pixel (Color (r, g, b)) =
     let !rByte = double2Int (255.999 * r)
         !gByte = double2Int (255.999 * g)
         !bByte = double2Int (255.999 * b)
-     in ImagePixel (rByte, gByte, bByte)
+    in ImagePixel (rByte, gByte, bByte)
 
-newtype Point3 = Point Vec3
-  deriving (Num) via Vec3
-  deriving (Vector3) via Vec3
+newtype Point3 = Point3 (Double, Double, Double)
+  deriving (Num, Vector3) via Vec3
+
+data Ray = Ray {origin :: Point3, direction :: Vec3}
+
+class ImageRay where
+  at :: Ray -> Double -> Point3
+  rayColor :: Ray -> Color
+
+instance ImageRay where
+  at ray t = ray.origin + coerce (ray.direction `scale` t)
+  rayColor ray =
+    let unitDirection = unitVector ray.direction
+        a = 0.5 * (yVal unitDirection + 1.0)
+    in Color (1.0, 1.0, 1.0) `scale` (1.0 - a) + Color (0.5, 0.7, 1.0) `scale` a
 
 type App =
   Eff
@@ -129,11 +150,54 @@ appRunner =
     . runLogger
     . runImage
 
+aspectRatio :: Double
+aspectRatio = 16.0 / 9.0
+
 imageWidth :: ImageWidth
-imageWidth = ImageWidth 256
+imageWidth = ImageWidth 400
 
 imageHeight :: ImageHeight
-imageHeight = ImageHeight 256
+imageHeight =
+  ImageHeight
+    $ max 1
+    $ double2Int
+    $ int2Double (coerce imageWidth) / aspectRatio
+
+viewportHeight :: ViewportHeight
+viewportHeight = ViewportHeight 2.0
+
+viewportWidth :: ViewportWidth
+viewportWidth =
+  ViewportWidth
+    $ coerce viewportHeight * (int2Double (coerce imageWidth) / int2Double (coerce imageHeight))
+
+focalLength :: Double
+focalLength = 1.0
+
+cameraCenter :: Point3
+cameraCenter = Point3 (0, 0, 0)
+
+viewportU :: Vec3
+viewportU = Vec3 (coerce viewportWidth, 0.0, 0.0)
+
+viewportV :: Vec3
+viewportV = Vec3 (0.0, -coerce viewportHeight, 0.0)
+
+pixelDeltaU :: Vec3
+pixelDeltaU = viewportU `scale` (1 / int2Double (coerce imageWidth))
+
+pixelDeltaV :: Vec3
+pixelDeltaV = viewportU `scale` (1 / int2Double (coerce imageHeight))
+
+viewportUpperLeft :: Point3
+viewportUpperLeft =
+  cameraCenter
+    - Point3 (0.0, 0.0, focalLength)
+    - coerce viewportU `scale` (1 / 2)
+    - coerce viewportV `scale` (1 / 2)
+
+pixel00Loc :: Point3
+pixel00Loc = viewportUpperLeft + coerce (pixelDeltaU + pixelDeltaV) `scale` 0.5
 
 main :: IO ()
 main = do
@@ -142,22 +206,22 @@ main = do
     Left (ImageHandleErr err) -> putTextLn $ "Error: " <> err
     Right _ -> pass
 
-createImage :: (Logger :> es, Image :> es) => Eff es ()
-createImage = E.bracket_ (openImage "image.ppm") closeImage $ do
-  initImage imageWidth imageHeight >> printImagePixels
+createImage :: (Image :> es, Logger :> es) => Eff es ()
+createImage =
+  E.bracket_ (openImage "image.ppm") closeImage
+    $ initImage imageWidth imageHeight >> printImagePixels
 
 printImagePixels :: (Image :> es, Logger :> es) => Eff es ()
 printImagePixels = withLog $ do
-  let ImageHeight imageHeight' = imageHeight
-      ImageWidth imageWidth' = imageWidth
-  j <- ListT.fromFoldable [0 .. imageHeight' - 1]
-  lift . log $ "\rScanlines remaining: " <> show (imageHeight' - j) <> " "
-  i <- ListT.fromFoldable [0 .. imageWidth' - 1]
-  let !r = int2Double i / int2Double (imageWidth' - 1)
-      !g = int2Double j / int2Double (imageHeight' - 1)
-      !b = 0.0
-      color = Color (Vec3 (r, g, b))
-  lift . putPixelLn . pixel $ color
-  where
-    withLog :: (Logger :> es) => ListT.ListT (Eff es) () -> Eff es ()
-    withLog listT = E.finally (void $ ListT.toList listT) (logLn "\rDone.                 ")
+  j <- ListT.fromFoldable [0 .. coerce imageHeight - 1]
+  lift . log $ "\rScanlines remaining: " <> show (coerce imageHeight - j) <> " "
+  i <- ListT.fromFoldable [0 .. coerce imageWidth - 1]
+  let pixelCenter =
+        pixel00Loc + coerce (pixelDeltaU `scale` int2Double i) + coerce (pixelDeltaV `scale` int2Double j)
+      rayDirection = coerce $ pixelCenter - cameraCenter
+      ray = Ray cameraCenter rayDirection
+      pixelColor = rayColor ray
+  lift . putPixelLn . pixel $ pixelColor
+ where
+  withLog :: Logger :> es => ListT.ListT (Eff es) () -> Eff es ()
+  withLog listT = E.finally (void $ ListT.toList listT) (logLn "\rDone.                 ")
